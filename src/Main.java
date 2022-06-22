c420ad471f6542329028cf244cd50ba6da5252cd
Nikolaeva Ekaterina <katy-forever@mail.ru>
2022-06-22 23:50:40 +0500

    Commit2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int balans = 100; // баланс до пополнения счета
-        int summa = 100; // сумма пополнения
+        int balance = 100; // баланс до пополнения счета
+        int amount = 100; // сумма пополнения
         int bonus; // начисленно бонусов
-        if (summa > 1000) {bonus = summa/100;
+        if (amount > 1000) {
+            bonus = amount / 100;
+        } else {
+            bonus = 0;
         }
-        else {bonus = 0;
-        }
-
-        System.out.println("Итого баланс = " + (balans + summa + bonus));
+        System.out.println("Итого баланс = " + (balance + amount + bonus));
     }
 }
